8c0a0a0da692347865c1e5e6b360b6195d489d2e
0jiiino <tnqkrtnqkr7756@gmail.com>
2024-09-02 16:04:42 +0900

    feat: 문제 리스트에 문제가 없을 경우, placeholder 화면 작업

--- src/components/problemSection/ProblemList.tsx
+++ src/components/problemSection/ProblemList.tsx
@@ -1,7 +1,6 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 import { ProblemContainer } from "./ProblemContainer";
-import { ProblemMapperInterface } from "../../services/problemMapper";
 import { getProblemList } from "../../services/problemApi";
 
 export const ProblemList = () => {
@@ -13,29 +12,46 @@ export const ProblemList = () => {
   if (isLoading) return null;
 
   return (
-    <section className="w-[480px] bg-[#5C5C5C] rounded-[12px] flex flex-col gap-[16px] px-[16px] pt-[17px]">
+    <section className="w-[480px] bg-[#5C5C5C] rounded-[12px] flex flex-col gap-[16px] px-[16px] pt-[17px] relative">
       <span className="flex tracking-[-1%] leading-[24px] text-[16px] font-bold text-white">
         학습지 상세 편집
       </span>
 
-      <section className="customScrollBar w-full flex flex-col gap-[16px] overflow-x-hidden overflow-y-scroll">
-        {data?.problemData?.map((problem, index) => (
-          <ProblemContainer
-            key={problem.id}
-            problemData={problem}
-            order={index + 1}
-          />
-        ))}
-      </section>
+      {!data?.problemData.length ? (
+        <div className="flex items-center justify-center text-center h-[calc(100%_-_85px)]">
+          <p className="leading-[21px] text-[14px] font-normal tracking-[-0.2%] text-white flex">
+            학습지 문제수가 없습니다.
+            <br />
+            다음단계로 넘어가기 위해 문제를 추가해주세요.
+          </p>
+          <p className="leading-[21px] text-[14px] font-normal tracking-[-0.2%] text-white flex"></p>
+        </div>
+      ) : (
+        <section className="customScrollBar w-full flex flex-col gap-[16px] overflow-x-hidden overflow-y-scroll pb-[70px]">
+          {data?.problemData?.map((problem, index) => (
+            <ProblemContainer
+              key={problem.id}
+              problemData={problem}
+              order={index + 1}
+            />
+          ))}
+        </section>
+      )}
 
-      <footer className="flex justify-end py-[20px] pr-[24px] tracking-[-1%]">
-        <span className="leading-[24px] text-[16px] font-normal text-white">
-          <span className="text-[12px] opacity-50">
-            {`하${data?.levelData[1]} · 중하${data?.levelData[2]} · 중${data?.levelData[3]} · 상${data?.levelData[4]} · 최상${data?.levelData[5]}`}
+      <footer className="absolute bottom-0 bg-[#5C5C5C] flex justify-end py-[20px] pr-[24px] tracking-[-1%] w-[calc(100%_-_32px)]">
+        {!data?.problemData.length ? (
+          <span className="leading-[24px] text-[16px] font-bold text-[#FD5354]">
+            문제 수 0개
           </span>
-          <span className="opacity-50">{" | "}</span>
-          {`문제 수 ${data?.problemData.length} 개`}
-        </span>
+        ) : (
+          <span className="leading-[24px] text-[16px] font-normal text-white">
+            <span className="text-[12px] opacity-50">
+              {`하${data?.levelData[1]} · 중하${data?.levelData[2]} · 중${data?.levelData[3]} · 상${data?.levelData[4]} · 최상${data?.levelData[5]}`}
+            </span>
+            <span className="opacity-50">{" | "}</span>
+            {`문제 수 ${data?.problemData.length} 개`}
+          </span>
+        )}
       </footer>
     </section>
   );
